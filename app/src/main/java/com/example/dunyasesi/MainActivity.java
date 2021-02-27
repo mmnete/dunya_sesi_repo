7baa389f98db060d7d9fcd80f2d75478d9d87c5f
Mohamed Mnete <mnetemohamed@gmail.com>
2021-02-26 20:17:04 -0600

    Changed main activity

--- app/src/main/java/com/example/dunyasesi/MainActivity.java
+++ app/src/main/java/com/example/dunyasesi/MainActivity.java
@@ -10,15 +10,13 @@ public class MainActivity extends AppCompatActivity {
     int myNumber1 = 10;
     int hewNumber = 100;
     int newChangeNumber = 1000000;
-<<<<<<< HEAD
-=======
+
 
     /*
       9utyegyhtireobuvneuivtrlhgdgldhr8yehgiut;hbuysebglisuh7rseghbr7tsebuigsblileghfwiojveurhfyu,bfilybyuefgyuekcubiulrhu
     
     
     */
->>>>>>> 0bcbd88a5ac26c5b24f4ece25eb49ae8e84c72f5
     // hey the code here does not work
 
     @Override
